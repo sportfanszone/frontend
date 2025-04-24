6bf69c6c3e1874ed67ff14dd6026e1ee9b3d70a7
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-04-24 10:43:11 +0100

    feat: create Home component with HeroSection integration

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import HeroSection from "./components/HeroSection";
+
+export default function Home() {
+  return (
+    <div>
+      <HeroSection />
+      {/* <h1>Font 1</h1>
+      <h1 className="font-montserrat">Font 2</h1> */}
+    </div>
+  );
+}
