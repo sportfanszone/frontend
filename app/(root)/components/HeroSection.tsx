59711d8ec6b6d46f0aea79aef4f20cf7039d5f24
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-05-03 04:42:01 +0100

    chore: refine layout and styling in HeroSection component

--- app/(root)/components/HeroSection.tsx
+++ app/(root)/components/HeroSection.tsx
@@ -2,16 +2,18 @@ import { FiSearch } from "react-icons/fi";
 
 const HeroSection = () => {
   return (
-    <section className="text-white relative bg-[url('/images/heroBackground.jpg')] bg-cover bg-center w-full h-screen ">
+    <section className="text-white relative bg-[url('/images/heroBackground.jpg')] bg-cover bg-center w-full h-screen">
       <div className="w-full h-screen bg-black/70 absolute top-0 left-0"></div>
-      <div className="w-full absolute top-[50%] left-[50%] transform -translate-x-[50%] -translate-y-[50%] text-center">
-        <h1 className="font-semibold text-6xl mb-5">find your dream home</h1>
-        <p className="mb-5">
+      <div className="w-full absolute top-[50%] left-[50%] transform -translate-x-[50%] -translate-y-[50%] text-center px-10">
+        <h1 className="font-semibold text-5xl md:text-6xl mb-5">
+          find your dream home
+        </h1>
+        <p className="mb-6 max-w-100 sm:max-w-full mx-auto text-sm md:text-lg font-light">
           Browse top listings, discover hidden gems, and move in with confidence
         </p>
-        <div className="bg-white text-black rounded-full w-fit m-auto flex justify-center items-center">
+        <div className="bg-white text-black rounded-full w-fit m-auto flex justify-center items-center mx-auto">
           <input
-            className="py-3.5 px-4 w-160 outline-none"
+            className="py-3.5 px-4 sm:w-100 md:w-160 outline-none"
             type="text"
             placeholder="Search your interest"
           />
