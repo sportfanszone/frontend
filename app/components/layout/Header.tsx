a54e73fa086d80314ec7e789788fe7268470d803
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-05-07 04:42:01 +0100

    chore: adjust padding in Header component for improved layout

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -4,7 +4,7 @@ import HeaderNav from "../ui/HeaderNav";
 const Header = () => {
   return (
     <>
-      <header className="flex justify-between items-center absolute w-full text-white py-7 px-17 z-1">
+      <header className="flex justify-between items-center absolute w-full text-white py-7 px-5 sm:px-10 md:px-17 z-1">
         <Image src="/images/logo.png" alt="Logo" width={160} height={160} />
         <HeaderNav />
       </header>
