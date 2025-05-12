8721cb6a5aa71729773c8c8c70b034c4c50757ef
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-05-12 04:42:01 +0100

    chore: add LeaguesSection component to Home for enhanced content display

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "./components/HeroSection";
+import LeaguesSection from "./components/LeaguesSection";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <LeaguesSection />
     </main>
   );
 }
