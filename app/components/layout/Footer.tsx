bc494b6030d023464999051395776d6988397051
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-05-23 00:20:14 +0100

    feat: add Footer component to layout for improved site structure

---
+++ app/components/layout/Footer.tsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white py-4">
+      <div className="container mx-auto text-center">
+        <p>
+          &copy; {new Date().getFullYear()} My Website. All rights reserved.
+        </p>
+        <p>
+          <a href="/privacy-policy" className="text-gray-400 hover:text-white">
+            Privacy Policy
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Montserrat } from "next/font/google";
 import "./globals.css";
 
 import Header from "@/app/components/layout/Header";
+import Footer from "@/app/components/layout/Footer";
 
 const montserrat = Montserrat({
   variable: "--font-montserrat",
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body className={`${montserrat.variable} antialiased font-montserrat`}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
