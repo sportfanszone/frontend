328adb033272a778838cd46cf648275ad31a71ef
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-05-30 22:58:04 +0100

    fix: improve layout and responsiveness of Login component

--- app/(auth)/auth/login/page.tsx
+++ app/(auth)/auth/login/page.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { FiEye, FiEyeOff } from "react-icons/fi";
+import ResponsiveIcon from "@/app/(auth)/components/ResponsiveIcon";
 
 export default function Login() {
   const [showPassword, setShowPassword] = useState(false);
@@ -11,39 +12,44 @@ export default function Login() {
   };
 
   return (
-    <main className="p-10 font-medium max-w-400 mx-auto min-h-screen pt-35">
-      <div className="text-center max-w-md mx-auto">
-        <h1 className="text-4xl font-semibold mb-10">Welcome Back</h1>
+    <main className="p-7 font-medium max-w-400 mx-auto min-h-screen pt-21 sm:pt-27 md:pt-35">
+      <div className="text-center max-w-xs sm:max-w-sm md:max-w-md mx-auto">
+        <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-6 sm:mb-7 md:mb-10">
+          Welcome Back
+        </h1>
 
         {/* Google button */}
-        <div className="flex items-center justify-center gap-4 mx-auto px-20 py-5 rounded-full border-2 border-black/20 hover:border-gray-500 transition-all duration-200 cursor-pointer mb-10">
+        <div className="flex items-center justify-center gap-4 mx-auto px-4 py-3 sm:py-4 md:py-5 rounded-full border-2 border-black/20 hover:border-gray-500 transition-all duration-200 cursor-pointer  mb-6 sm:mb-7 md:mb-10">
           <Image
+            className="width-6 h-6 sm:w-6 sm:h-6 md:w-7.5 md:h-7.5"
             src="/svgs/google-icon-logo.svg"
             alt="Google logo"
             width={25}
             height={25}
           />
-          <span className="text-lg font-bold">Login with Google</span>
+          <span className="text-sm sm:text-md md:text-lg font-bold">
+            Login with Google
+          </span>
         </div>
 
         {/* Divider */}
-        <div className="flex items-center justify-center gap-4 mb-10">
+        <div className="flex items-center justify-center gap-4 mb-6 sm:mb-7 md:mb-10">
           <hr className="border-b w-full border-black/20" />
-          <span>or</span>
+          <span className="md:text-lg">or</span>
           <hr className="border-b w-full border-black/20" />
         </div>
 
         {/* Form */}
         <form action="">
-          <div className="border-2 border-black/20 rounded-full relative bg-black/5 mb-8.5">
+          <div className="border-2 border-black/20 rounded-full relative bg-black/3 mb-5 sm:mb-6 md:mb-8.5">
             <label
-              className="text-lg font-bold absolute top-0 left-7 -translate-y-[50%]"
+              className="text-sm sm:text-md md:text-lg font-bold absolute top-0 left-4 md:left-7 -translate-y-[50%]"
               htmlFor="email"
             >
               Email
             </label>
             <input
-              className="w-full px-4 py-4 outline-none bg-transparent"
+              className="text-xs sm:text-md md:text-lg w-full p-3 sm:p-3 md:p-4 outline-none bg-transparent"
               id="email"
               type="email"
               placeholder="Your Eamil"
@@ -51,26 +57,26 @@ export default function Login() {
           </div>
           <div className="flex items-center justify-between border-2 border-black/20 rounded-full relative bg-black/5 mb-8.5">
             <label
-              className="text-lg font-bold absolute top-0 left-7 -translate-y-[50%]"
+              className="text-sm sm:text-md md:text-lg font-bold absolute top-0 left-4 md:left-7 -translate-y-[50%]"
               htmlFor="password"
             >
               Password
             </label>
             <input
-              className="w-full px-4 py-4 outline-none bg-transparent"
+              className="text-xs sm:text-md md:text-lg w-full p-3 sm:p-3 md:p-4 outline-none bg-transparent"
               id="password"
               type={showPassword ? "text" : "password"}
               placeholder="Your Password"
             />
             <button
-              className="text-xl font-bold p-2 mr-2 cursor-pointer"
+              className="text-md sm:text-lg md:text-xl font-bold p-2 mr-2 cursor-pointer"
               type="button"
               onClick={handleShowPassword}
             >
               {showPassword ? (
-                <FiEye strokeWidth={3} />
+                <ResponsiveIcon Icon={FiEye} className="text-inherit" />
               ) : (
-                <FiEyeOff strokeWidth={3} />
+                <ResponsiveIcon Icon={FiEyeOff} className="text-inherit" />
               )}
             </button>
           </div>
@@ -85,20 +91,20 @@ export default function Login() {
                 type="checkbox"
                 className="w-5 h-5 accent-green-600 border-gray-300 rounded focus:ring-green-500"
               />
-              <span className="text-lg">Remember me</span>
+              <span className="text-sm sm:text-md md:text-lg">Remember me</span>
             </label>
 
-            <Link className="text-lg text-green-500" href="#">
+            <Link className="text-green-500" href="#">
               Forgot Password
             </Link>
           </div>
         </form>
 
-        <button className="text-white flex items-center justify-center gap-4 w-full mx-auto p-4 rounded-full bg-green-500 transition-all duration-200 cursor-pointer mb-10">
-          <span className="text-lg font-bold">Login</span>
+        <button className="text-white text-sm sm:text-md md:text-lg font-bold flex items-center justify-center gap-4 w-full mx-auto p-3 sm:p-3 md:p-4 rounded-full bg-green-500 transition-all duration-200 cursor-pointer  mb-7 sm:mb-8 md:mb-10">
+          Login
         </button>
 
-        <div className="font-bold flex items-center justify-center gap-4 mb-6">
+        <div className="text-sm sm:text-md md:text-lg font-bold flex items-center justify-center gap-4 mb-6">
           <span>Don't have an account?</span>
 
           <Link className="text-green-500" href="/auth/signup">
