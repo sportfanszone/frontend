604bd31150da49fe9011cf6f304e4116b3ae40b3
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-06-01 05:27:59 +0100

    fix: adjust max width of LeaguesSection component

--- app/(root)/components/LeaguesSection.tsx
+++ app/(root)/components/LeaguesSection.tsx
@@ -66,7 +66,7 @@ const leagues = [
 
 const LeaguesSection = () => {
   return (
-    <section className="p-10 font-medium max-w-400 mx-auto">
+    <section className="p-10 font-medium max-w-300 mx-auto">
       <h2 className="font-bold text-2xl sm:text-3xl md:text-4xl text-center mb-10">
         Leagues
       </h2>
