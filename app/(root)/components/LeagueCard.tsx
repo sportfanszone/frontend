7b5a65fed09bf01aa2f2da0f2f1cf73ed4671881
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-06-01 05:05:01 +0100

    feat: add background images and update league details in LeagueCard and LeaguesSection components

--- app/(root)/components/LeagueCard.tsx
+++ app/(root)/components/LeagueCard.tsx
@@ -7,6 +7,7 @@ type LeagueCardProps = {
   lastActivity: string;
   description: string;
   logo: string;
+  backgroundImage: string;
 };
 
 const LeagueCard = ({
@@ -15,10 +16,15 @@ const LeagueCard = ({
   lastActivity,
   description,
   logo,
+  backgroundImage,
 }: LeagueCardProps) => {
   return (
     <motion.div className="shadow-card hover:shadow-card-active hover:scale-102 rounded-3xl p-6 max-w-80 min-w-66 cursor-pointer transition-all duration-150 ease-in-out">
-      <div className="mb-4 shadow-card rounded-3xl px-5 py-6 text-white bg-green-500">
+      {/* <div className="mb-4 shadow-card rounded-3xl px-5 py-6 text-white bg-[#37003C]"> */}
+      <div
+        className={`mb-4 shadow-card rounded-3xl px-5 py-6 text-white bg-cover bg-center`}
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+      >
         <h1 className="font-bold text-xl">{leagueName}</h1>
       </div>
       <div className="flex justify0between align-center gap-3">
@@ -31,7 +37,7 @@ const LeagueCard = ({
           <span className="font-bold text-lg">{lastActivity}</span>
         </div>
       </div>
-      <div className="flex flex-col justify-center mb-4">
+      <div className="flex flex-col justify-center">
         <span className="text-gray-500 text-sm mb-1">DESCRIPTION</span>
         <div className="flex justify-start items-center gap-3">
           <Image
