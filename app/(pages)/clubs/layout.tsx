4b21f31e81cb115164ed3b7904f059b54a94434c
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-06-02 21:55:54 +0100

    feat: refactor LeagueClubsLayout to use ClubsSidebar and update data fetching

--- app/(pages)/clubs/layout.tsx
+++ app/(pages)/clubs/layout.tsx
@@ -1,13 +1,31 @@
 import ClubsSidebar from "@/app/(pages)/components/ClubsSidebar";
+import React, { ReactElement } from "react";
 
-export default function LeagueClubsLayout({
+type PageData = {
+  posts: number;
+  followers: number;
+};
+
+async function getData(): Promise<PageData> {
+  return {
+    posts: 10,
+    followers: 200,
+  };
+}
+
+export default async function ClubsLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: ReactElement<{ data: PageData }>;
 }) {
+  const data = await getData();
+  const childrenWithProps = React.cloneElement(children, { data });
+
   return (
     <>
-      <div className="min-h-screen w-[100%] px-4 py-10">{children}</div>
+      <div className="min-h-screen w-[100%] px-4 py-10">
+        {childrenWithProps}
+      </div>
       {/* Sidebar */}
       <div className="hidden md:flex min-h-screen w-120 mr-4 mt-4">
         <ClubsSidebar />
