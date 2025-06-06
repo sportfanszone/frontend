758ec9dcca1acbf2f507cf93d100f6714c03ec55
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-06-06 21:40:08 +0100

    feat: update section title from 'Latest Posts' to 'Latest Conversations'

--- app/(root)/components/LatestPostsSection.tsx
+++ app/(root)/components/LatestPostsSection.tsx
@@ -85,7 +85,7 @@ const LatestPosts = () => {
   return (
     <section className="p-10 font-medium max-w-300 mx-auto @container">
       <h2 className="font-bold text-2xl sm:text-3xl md:text-4xl text-center mb-10">
-        Latest Posts
+        Latest Conversations
       </h2>
 
       <div className="flex justify-center items-center flex-wrap gap-8">
