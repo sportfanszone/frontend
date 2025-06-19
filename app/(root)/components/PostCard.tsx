80d5916d67ff32bc0022c4571b4876eaf33b8c51
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-06-19 16:31:26 +0100

    refactor: enhance layout and styling of PostCard component for improved readability and user experience

--- app/(root)/components/PostCard.tsx
+++ app/(root)/components/PostCard.tsx
@@ -29,40 +29,45 @@ const PostCard = ({
   createdAt,
 }: PostCardProps) => {
   return (
-    <div className="shadow-card hover:shadow-card-active hover:scale-102 rounded-3xl p-6 max-w-80 min-w-66 cursor-pointer transition-all duration-150 ease-in-out">
-      <div className="flex justify-start items-center gap-3 mb-4">
-        <Image
-          src={porofileImage}
-          width={200}
-          height={200}
-          alt="User porofile image"
-          className="h-10 w-10 object-cover rounded-full"
-        />
-        <div className="flex flex-col justify-start items-center gap-0.5">
-          <span className="text-sm font-bold ">{username}</span>
-          <span className="text-sm text-gray-500">{createdAt}</span>
-        </div>
-      </div>
+    <div className="flex items-center justify-between gap-4 max-h-10em max-w-130 shadow-card hover:shadow-card-active hover:scale-102 rounded-3xl p-6 cursor-pointer transition-all duration-150 ease-in-out">
       <Image
         src="/images/postImage1.jpg"
-        width={200}
-        height={200}
+        width={170}
+        height={170}
         alt="Post Image"
-        className="h-30 w-full object-cover rounded-md mb-4"
+        className="w-[10.5em] h-[10.5em] object-cover rounded-3xl"
       />
-      <h5 className="font-bold text-lg">{title}</h5>
-      <p className="text-sm mb-4">{description}</p>
-      <div className="flex justify-between items-center">
-        <div className="flex justify-between items-center gap-5">
-          <div className="flex justify-between items-center gap-1">
-            <FiThumbsUp /> <b>{likes}</b>
-          </div>
-          <div className="flex justify-between items-center gap-1">
-            <FiMessageCircle /> <b>{comments}</b>
+      <div className="min-w-[1em] w-[18em] mxa-h-[10.5em]">
+        <div className="flex justify-start items-center gap-3 mb-2 md:mb-4">
+          <Image
+            src={porofileImage}
+            width={200}
+            height={200}
+            alt="User porofile image"
+            className="h-7 md:h-10 w-7 md:w-10 object-cover rounded-full"
+          />
+          <div className="flex flex-col justify-start items-center gap-0.5">
+            <span className="text-xs md:text-sm font-bold ">{username}</span>
+            <span className="text-xs md:text-sm text-gray-500">
+              {createdAt}
+            </span>
           </div>
         </div>
-        <div className="flex justify-between items-center gap-2">
-          <FiChevronUp /> <b>{upVotes}</b> <FiChevronDown />
+
+        <h5 className="font-bold text-md md:text-lg">{title}</h5>
+        <p className="text-sm mb-2 md:mb-4 line-clamp-2">{description}</p>
+        <div className="flex justify-between items-center text-sm md:text-base">
+          <div className="flex justify-between items-center gap-5">
+            <div className="flex justify-between items-center gap-1">
+              <FiThumbsUp /> <b>{likes}</b>
+            </div>
+            <div className="flex justify-between items-center gap-1">
+              <FiMessageCircle /> <b>{comments}</b>
+            </div>
+          </div>
+          <div className="flex justify-between items-center gap-2">
+            <FiChevronUp /> <b>{upVotes}</b> <FiChevronDown />
+          </div>
         </div>
       </div>
     </div>
