bd5ae62716f5a251afc2c04132af26e4708c9b6d
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-06-20 05:58:02 +0100

    feat: enhance TopicCard component with likes, comments, and upVotes display

--- app/(pages)/components/TopicCard.tsx
+++ app/(pages)/components/TopicCard.tsx
@@ -1,10 +1,19 @@
 import Image from "next/image";
+import {
+  FiChevronDown,
+  FiChevronUp,
+  FiMessageCircle,
+  FiThumbsUp,
+} from "react-icons/fi";
 
 type TopicCardProps = {
   topic: {
     id: number;
     title: string;
     createdAt: string;
+    likes: number;
+    comments: number;
+    upVotes: number;
     user: {
       firstName: string;
       middleName: string;
@@ -19,26 +28,55 @@ const TopicCard = ({
     id,
     title,
     createdAt,
+    likes,
+    comments,
+    upVotes,
     user: { firstName, middleName, lastName, profileImage },
   },
 }: TopicCardProps) => {
   return (
-    <div className="flex items-center justify-center gap-4 p-4">
+    <div className="flex gap-4 shadow-card hover:shadow-card-active hover:scale-102 rounded-3xl p-6 cursor-pointer transition-all duration-150 ease-in-out">
       <Image
-        src={profileImage}
-        className="rounded-full w-10 h-10 object-cover"
-        alt="User Profile"
-        width={100}
-        height={100}
+        src="/images/postImage1.jpg"
+        width={170}
+        height={170}
+        alt="Post Image"
+        className="w-[9.5em] h-[9.5em] object-cover rounded-3xl"
       />
-      <div>
-        <div>
-          <span className="font-bold mr-3">
-            {firstName} {middleName} {lastName}
-          </span>
-          <span className="text-black/40">{createdAt}</span>
+
+      <div className="min-w-[1em]">
+        <h5 className="font-bold text-md md:text-lg line-clamp-2 mb-2">
+          {title}
+        </h5>
+        <div className="flex justify-between items-center gap-5 md:mb-4 max-w-40">
+          <div className="flex justify-between items-center gap-1">
+            <FiThumbsUp /> <b>{likes}</b>
+          </div>
+          <div className="flex justify-between items-center gap-1">
+            <FiMessageCircle /> <b>{comments}</b>
+          </div>
+          <div className="flex justify-between items-center gap-2">
+            <FiChevronUp /> <b>{upVotes}</b> <FiChevronDown />
+          </div>
         </div>
-        <div className="text-black/40">{title}</div>
+
+        <div className="flex justify-start items-center gap-3">
+          <Image
+            src={profileImage}
+            width={200}
+            height={200}
+            alt="User porofile image"
+            className="h-7 md:h-10 w-7 md:w-10 object-cover rounded-full"
+          />
+          <div className="flex flex-col justify-start items-start gap-0.5">
+            <span className="text-xs md:text-sm font-bold ">
+              {firstName} {middleName} {lastName}
+            </span>
+            <span className="text-xs md:text-sm text-gray-500">
+              {createdAt}
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
--- app/(pages)/topics/page.tsx
+++ app/(pages)/topics/page.tsx
@@ -6,7 +6,10 @@ const getTopics = async () => {
     {
       id: 1,
       title:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis, tempore.",
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam.",
+      likes: 10,
+      comments: 5,
+      upVotes: 20,
       createdAt: "10:02AM",
       user: {
         firstName: "John",
@@ -17,8 +20,10 @@ const getTopics = async () => {
     },
     {
       id: 2,
-      title:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis, tempore.",
+      title: "Lorem ipsum dolor sit, amet consectetur adipisicing.",
+      likes: 10,
+      comments: 5,
+      upVotes: 20,
       createdAt: "10:02AM",
       user: {
         firstName: "John",
@@ -29,8 +34,10 @@ const getTopics = async () => {
     },
     {
       id: 3,
-      title:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis, tempore.",
+      title: "Lorem ipsum dolor sit, amet consectetur adipisicing.",
+      likes: 10,
+      comments: 5,
+      upVotes: 20,
       createdAt: "10:02AM",
       user: {
         firstName: "John",
@@ -42,7 +49,10 @@ const getTopics = async () => {
     {
       id: 1,
       title:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis, tempore.",
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit, voluptatum.",
+      likes: 10,
+      comments: 5,
+      upVotes: 20,
       createdAt: "10:02AM",
       user: {
         firstName: "John",
@@ -53,8 +63,10 @@ const getTopics = async () => {
     },
     {
       id: 2,
-      title:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis, tempore.",
+      title: "Lorem ipsum dolor sit, amet consectetur adipisicing elit, tum.",
+      likes: 10,
+      comments: 5,
+      upVotes: 20,
       createdAt: "10:02AM",
       user: {
         firstName: "John",
@@ -66,7 +78,10 @@ const getTopics = async () => {
     {
       id: 3,
       title:
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis, tempore.",
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam, voluptatum.",
+      likes: 10,
+      comments: 5,
+      upVotes: 20,
       createdAt: "10:02AM",
       user: {
         firstName: "John",
@@ -82,17 +97,18 @@ export default async function TopicsPage() {
   const topics = await getTopics();
   return (
     <main className="font-medium max-w-400 mx-auto px-4">
-      <section className=" max-w-300 mx-auto">
-        <h2 className="font-bold text-2xl sm:text-3xl md:text-4xl text-center mb-10">
+      <section className="w-full max-w-300 mx-auto">
+        <h2 className="font-bold text-2xl sm:text-3xl md:text-4xl mb-10">
           Topics
         </h2>
 
-        {topics.map((topic, index) => (
-          <Link href={`/post/${topic.id}`} key={index}>
-            <TopicCard topic={topic} />
-          </Link>
-        ))}
-        <div></div>
+        <div className="flex flex-col gap-6 max-w-250 mx-auto w-full">
+          {topics.map((topic, index) => (
+            <Link href={`/post/${topic.id}`} key={index}>
+              <TopicCard topic={topic} />
+            </Link>
+          ))}
+        </div>
       </section>
     </main>
   );
