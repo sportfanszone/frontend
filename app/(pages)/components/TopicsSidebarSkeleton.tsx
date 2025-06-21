f8786973b3321e6d0735fd3f55cf6688cf0267ce
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-06-21 14:24:52 +0100

    feat: update TopicsSidebarSkeleton component to enhance loading skeleton width for better visual consistency

--- app/(pages)/components/TopicsSidebarSkeleton.tsx
+++ app/(pages)/components/TopicsSidebarSkeleton.tsx
@@ -6,16 +6,10 @@ const TopicsSidebarSkeleton = () => {
     <div className="border-2 border-gray-200 rounded-xl w-full h-fit">
       {/* Top contributors */}
       <div className="p-6">
-        <Skeleton className="mb-4" height={20} width={100} />
+        <Skeleton className="mb-4" height={20} width={130} />
         <div className="flex items-center justify-center-safe flex-wrap gap-4">
           {[...Array(6)].map((_, index) => (
-            <Skeleton
-              key={index}
-              //   className="w-16 h-16 rounded-full"
-              width={50}
-              height={50}
-              circle={true}
-            />
+            <Skeleton key={index} width={50} height={50} circle={true} />
           ))}
         </div>
       </div>
