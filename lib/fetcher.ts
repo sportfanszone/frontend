c04917ab2b99469875442395ac6b6ed7f56ab587
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-06-27 02:34:11 +0100

    fix: enhance fetcher to include cookie header and improve data handling

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,16 +1,21 @@
+import { cookies } from "next/headers";
+
 type FetchAction = "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
 
 export default async function fetcher<T>(
   url: string,
-  action: FetchAction = "GET"
+  action: FetchAction = "GET",
+  data?: any
 ): Promise<T> {
+  const myCookies = await cookies();
+  const cookieHeader = myCookies.toString();
   const res = await fetch(url, {
     method: action,
     headers: {
       "Content-Type": "application/json",
+      Cookie: cookieHeader,
     },
-    body: action !== "GET" ? JSON.stringify({}) : undefined,
-    credentials: "include",
+    body: action !== "GET" && data ? JSON.stringify(data) : undefined,
   });
 
   if (!res.ok) {
