5e18809ee5bc3a8a48e82d468e550a03c0ba3a69
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-06-28 13:36:38 +0100

    fix: remove unused getDashboardData import and related code from UserDashboard

--- app/(user)/user/dashboard/page.tsx
+++ app/(user)/user/dashboard/page.tsx
@@ -1,10 +1,8 @@
 import ProfileHeader from "../../components/ProfileHeader";
 import { getUserFromCookie } from "@/lib/auth";
-// import getDashboardData from "@/lib/getDashboardData";
 import { User } from "@/types";
 
 export default async function UserDashboard() {
-  //   const data = await getDashboardData();
   const user = await getUserFromCookie();
 
   return (
