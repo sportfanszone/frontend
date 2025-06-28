4e2c490c2595c859cc5398a47a67327a2b697d91
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-06-28 14:34:38 +0100

    fix: remove commented-out collapsible navigation from LeftSidebar component

--- app/(pages)/components/LeftSidebar.tsx
+++ app/(pages)/components/LeftSidebar.tsx
@@ -114,51 +114,6 @@ const LeftSidebar = ({ user }: LeftSidebarProps) => {
                 </>
               )}
             </nav>
-
-            {/* Colapsable nav */}
-            {/* <div className="flex flex-col gap-2">
-              <div className="text-md font-semibold text-gray-700 flex items-center justify-start gap-2 cursor-pointer">
-                <span>Resources</span> <FiChevronDown />
-              </div>
-              <nav className="flex flex-col gap-2 pl-3">
-                <div className="flex items-center gap-2 text-md font-semibold">
-                  <FiInfo className="inline-block mr-2 text-gray-700" />
-                  <a href="/" className="text-gray-700 hover:text-black">
-                    About SFZ
-                  </a>
-                </div>
-                <div className="flex items-center gap-2 text-md font-semibold">
-                  <FiUsers className="inline-block mr-2 text-gray-700" />
-                  <a href="#" className="text-gray-700 hover:text-black">
-                    Advertisement
-                  </a>
-                </div>
-                <div className="flex items-center gap-2 text-md font-semibold">
-                  <FiHelpCircle className="inline-block mr-2 text-gray-700" />
-                  <a href="#" className="text-gray-700 hover:text-black">
-                    Help
-                  </a>
-                </div>
-                <div className="flex items-center gap-2 text-md font-semibold">
-                  <FiHelpCircle className="inline-block mr-2 text-gray-700" />
-                  <a href="#" className="text-gray-700 hover:text-black">
-                    FAQs
-                  </a>
-                </div>
-                <div className="flex items-center gap-2 text-md font-semibold">
-                  <FiShield className="inline-block mr-2 text-gray-700" />
-                  <a href="#" className="text-gray-700 hover:text-black">
-                    Privacy Policy
-                  </a>
-                </div>
-                <div className="flex items-center gap-2 text-md font-semibold">
-                  <FiFileText className="inline-block mr-2 text-gray-700" />
-                  <a href="#" className="text-gray-700 hover:text-black">
-                    Terms of Service
-                  </a>
-                </div>
-              </nav>
-            </div> */}
           </div>
         </div>
       </div>
