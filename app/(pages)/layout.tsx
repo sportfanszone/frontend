6a740b56af37afcf69c17782d22e9458c3ee849d
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-06-28 18:10:49 +0100

    refactor: cast user as User type in LeftSidebar component

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -2,6 +2,7 @@ import Header from "@/app/(pages)/components/Header";
 import LeftSidebar from "@/app/(pages)/components/LeftSidebar";
 import { SidebarProvider } from "@/app/(pages)/context/SideBarContext";
 import { getUserFromCookie } from "@/lib/auth";
+import { User } from "@/types";
 
 export default async function Layout({
   children,
@@ -19,7 +20,7 @@ export default async function Layout({
         <div className="flex items-start min-h-full">
           <div className="flex justify-between w-[100%]">
             {/* Left Sidebar */}
-            <LeftSidebar user={user} />
+            <LeftSidebar user={user as User} />
 
             {children}
           </div>
