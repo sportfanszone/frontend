0a4177fdc8b333df7c1e9d0ca9d3f528c06fe2f5
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-07-04 18:46:08 +0100

    refactor: update club data structure and import path in LeagueSection component

--- app/(pages)/leagues/[id]/page.tsx
+++ app/(pages)/leagues/[id]/page.tsx
@@ -1,11 +1,11 @@
-import ClubCard from "@/app/(pages)/components/clubCard";
+import ClubCard from "@/app/(pages)/components/clubs/ClubCard";
 import Link from "next/link";
 
-const club = [
+const clubs = [
   {
-    id: 1,
-    leagueName: "Manchester City",
-    topics: 142,
+    id: "1",
+    name: "Manchester City",
+    topicCount: 142,
     lastActivity: "Today, 09:14",
     description:
       "The most competitive football league in the world, featuring top English clubs.",
@@ -13,9 +13,9 @@ const club = [
     backgroundImage: "/images/manchesterCityBackgroundImage.png",
   },
   {
-    id: 2,
-    leagueName: "Arsenal",
-    topics: 95,
+    id: "2",
+    name: "Arsenal",
+    topicCount: 95,
     lastActivity: "Yesterday, 22:08",
     description:
       "Spain’s top-tier league, known for its flair, rivalries, and legendary players.",
@@ -23,9 +23,9 @@ const club = [
     backgroundImage: "/images/arsenalBackgroundImage.png",
   },
   {
-    id: 3,
-    leagueName: "Liverpool",
-    topics: 110,
+    id: "3",
+    name: "Liverpool",
+    topicCount: 110,
     lastActivity: "Today, 06:45",
     description:
       "Germany’s premier league, combining strong fan culture and attacking football.",
@@ -33,9 +33,9 @@ const club = [
     backgroundImage: "/images/liverpoolBackgroundImage.png",
   },
   {
-    id: 4,
-    leagueName: "Chelsea",
-    topics: 87,
+    id: "4",
+    name: "Chelsea",
+    topicCount: 87,
     lastActivity: "2 days ago, 18:33",
     description:
       "Italy’s historic league, home to tactical brilliance and legendary defenders.",
@@ -43,9 +43,9 @@ const club = [
     backgroundImage: "/images/chelseaBackgroundImage.png",
   },
   {
-    id: 5,
-    leagueName: "Manchester United",
-    topics: 74,
+    id: "5",
+    name: "Manchester United",
+    topicCount: 74,
     lastActivity: "Today, 11:01",
     description:
       "France’s top league, spotlighting young talents and dominant PSG performances.",
@@ -53,9 +53,9 @@ const club = [
     backgroundImage: "/images/manchesterUnitedBackgroundImage.png",
   },
   {
-    id: 6,
-    leagueName: "Tottenham Hotspur",
-    topics: 63,
+    id: "6",
+    name: "Tottenham Hotspur",
+    topicCount: 63,
     lastActivity: "Yesterday, 20:17",
     description:
       "North America’s growing league, blending global stars and rising talents.",
@@ -73,15 +73,16 @@ export default function LeagueSection() {
         </h2>
 
         <div className="flex justify-center items-center flex-wrap gap-8">
-          {club.map((league, index) => (
-            <Link href={`/club/${league.id}`} key={index}>
+          {clubs.map((club, index) => (
+            <Link href={`/club/${club.id}`} key={index}>
               <ClubCard
-                clubName={league.leagueName}
-                topics={league.topics}
-                lastActivity={league.lastActivity}
-                description={league.description}
-                logo={league.logo}
-                backgroundImage={league.backgroundImage}
+                id={club.id}
+                name={club.name}
+                topicCount={club.topicCount}
+                lastActivity={club.lastActivity}
+                description={club.description}
+                logo={club.logo}
+                backgroundImage={club.backgroundImage}
               />
             </Link>
           ))}
