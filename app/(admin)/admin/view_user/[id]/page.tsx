f2c6a9e322fd90b92d0748f69f38520ecf4ac8a5
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-07-09 07:38:40 +0100

    feat: add user view page with AddUserForm component

---
+++ app/(admin)/admin/view_user/[id]/page.tsx
@@ -0,0 +1,15 @@
+import AddUserForm from "@/app/(admin)/components/add_user/AddUserForm";
+
+export default function Page() {
+  return (
+    <div className="flex flex-1 flex-col">
+      <div className="@container/main flex flex-1 flex-col gap-2">
+        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+          <div className="px-4 lg:px-6">
+            <AddUserForm />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
