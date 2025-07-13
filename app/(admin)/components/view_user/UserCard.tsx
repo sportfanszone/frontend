04f2b7e76dd076020da712c07fb8123788bce65c
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-07-13 02:34:11 +0100

    feat: add UserCard component for user profile display in view_user page

---
+++ app/(admin)/components/view_user/UserCard.tsx
@@ -0,0 +1,26 @@
+import UserAvatar from "@/app/components/ui/UserAvatar";
+
+const UserCard = () => {
+  return (
+    <>
+      {/* Profile photo and cover */}
+      <div
+        style={{ backgroundImage: `url("https://placehold.co/900x200")` }}
+        className="bg-cover bg-center h-48 w-full flex items-center justify-center relative"
+      >
+        <div className="flex flex-col items-center justify-center absolute bottom-[-50%] left-10">
+          <div className="relative w-32 h-32 mb-4">
+            <UserAvatar
+              alt="User Avatar"
+              className="rounded-full object-cover"
+            />
+          </div>
+          <h2 className="text-xl font-semibold">John Doe</h2>
+          <p className="text-gray-500"></p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default UserCard;
