485a7450339ed59919e96d949efc78a6508750eb
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-07-15 02:34:11 +0100

    fix: update layout and spacing in ProfileHeader component for improved user display

--- app/(user)/components/dashboard/ProfileHeader.tsx
+++ app/(user)/components/dashboard/ProfileHeader.tsx
@@ -5,16 +5,14 @@ import { ProfileHeaderProps } from "@/types";
 
 const ProfileHeader = ({ user }: ProfileHeaderProps) => {
   return (
-    <div className="relative mb-12">
+    <div className="relative mb-30">
       <div
         className={`h-30 sm:h-40 md:h-45 rounded-2xl overflow-hidden bg-cover bg-center`}
         style={{ backgroundImage: `url(${user?.coverPhotoUrl})` }}
-      >
-        <div className="w-full h-full bg-gradient-to-t from-black/90 from-10% to-transparent to-90%" />
-      </div>
+      ></div>
 
-      <div className="flex items-center flex-wrap absolute bottom-0 left-5 sm:left-7 md:left-8 lg:left-10 translate-y-[50%]">
-        <div className="h-20 min-h-20 sm:h-28 sm:min-h-28 md:h-30 md:min-h-30 w-20 min-w-20 sm:w-28 sm:min-w-28 md:w-30 md:min-w-30  rounded-full overflow-hidden">
+      <div className="flex items-center flex-wrap gap-2 md:gap-4 absolute bottom-[-65%] sm:bottom-[-55%] md:bottom-[-50%] left-5 sm:left-7 md:left-8 lg:left-10 w-full">
+        <div className="h-25 min-h-25 w-25 sm:h-28 sm:min-h-28 md:h-30 md:min-h-30 min-w-20 sm:w-28 sm:min-w-28 md:w-30 md:min-w-30 rounded-full overflow-hidden">
           <UserAvatar
             alt={`${user.firstName} ${user.middleName} ${user.lastName}`}
             src={user.profileImageUrl}
@@ -22,19 +20,19 @@ const ProfileHeader = ({ user }: ProfileHeaderProps) => {
             width={500}
             height={500}
           />
-        </div>
 
-        <div>
-          <p className="font-bold text-base md:text-xl text-white p-2 py-2 sm:p-3 md:p-4">
-            {user?.firstName} {user?.middleName} {user?.lastName}
-          </p>
-          <div className="text-black/90 text-xs sm:text-sm flex items-center gap-4 sm:gap-5 md:gap-6 p-2 py-1 sm:p-3 md:p-4">
-            <div>12.2k Followers</div>
-            <div>1.2k Following</div>
-            <div className="flex  items-center gap-4">
-              <AchievementCards achievements={user.achievements} /> Achievements
+          <div className="absolute top-[30%] left-25 sm:left-28 md:left-30 max-w-max ml-3 mr-9">
+            <p className="font-bold text-base md:text-xl leading-tight truncate">
+              {user?.firstName} {user?.middleName} {user?.lastName}
+            </p>
+            <div className="text-black/90 text-xs sm:text-sm flex items-center flex-wrap gap-4">
+              <div>12.2k Followers</div>
+              <div>1.2k Following</div>
+              <div className="flex  items-center gap-4">
+                <AchievementCards achievements={user.achievements} />{" "}
+                Achievements
+              </div>
             </div>
-            {/* <div>Joined 2nd Jan 1972</div> */}
           </div>
         </div>
       </div>
