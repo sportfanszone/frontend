43d87cf70fde52d8c6dc51ec8407916d7190cab5
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-07-23 21:35:34 +0100

    refactor: fix DropzoneUploader location

--- app/(admin)/components/add_club/AddClubForm.tsx
+++ app/(admin)/components/add_club/AddClubForm.tsx
@@ -5,7 +5,7 @@ import InputGroup from "@/app/components/ui/InputGroup";
 import SelectGroup from "@/app/(admin)/components/add_club/SelectGroup";
 import { addClubSchema } from "@/lib/validation/addClubSchema";
 import { Button } from "@/app/components/ui/button";
-import DropzoneUploader from "@/app/components/ui/DropzoneUploader";
+import DropzoneUploader from "@/app/(admin)/components/DropzoneUploader";
 
 import { League } from "@/types";
 import clientFetcher from "@/lib/clientFetcher";
