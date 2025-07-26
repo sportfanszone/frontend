9d2e4d179f6a8368f09796c515aee8faf78bd4a6
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-07-26 04:52:58 +0100

    feat: add BackButton to Topics page and clean up unused code

--- app/(pages)/topics/page.tsx
+++ app/(pages)/topics/page.tsx
@@ -1,7 +1,7 @@
-import Link from "next/link";
 import TopicCard from "@/app/(pages)/components/topics/TopicCard";
 import getTopicsData from "@/lib/getTopicsData";
 import { TopicPageData } from "@/types";
+import BackButton from "@/app/components/ui/BackButton";
 
 export default async function TopicsPage() {
   try {
@@ -14,22 +14,22 @@ export default async function TopicsPage() {
     return (
       <main className="font-medium max-w-400 mx-auto px-4">
         <section className="w-full max-w-300 mx-auto">
-          <h2 className="font-bold text-2xl sm:text-3xl md:text-4xl mb-10">
-            Topics
-          </h2>
+          <div className="flex gap-3">
+            <BackButton />
+            <h2 className="font-bold text-2xl sm:text-3xl md:text-4xl mb-10">
+              Topics
+            </h2>
+          </div>
 
           <div className="flex flex-col gap-6 max-w-250 mx-auto w-full">
             {topics.map((topic, index) => (
-              // <Link href={`/post/${topic.id}`} key={index}>
               <TopicCard key={index} topic={topic} />
-              // </Link>
             ))}
           </div>
         </section>
       </main>
     );
   } catch (error) {
-    console.error("Failed to fetch topics:", error);
     return <p>An error occured while loading topics</p>;
   }
 }
