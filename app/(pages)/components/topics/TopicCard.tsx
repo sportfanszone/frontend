35ab69ddbe6204c95522f71ca5641999cc36e17b
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-07-26 05:57:59 +0100

    refactor: enhance TopicCard with improved layout and user profile links

--- app/(pages)/components/topics/TopicCard.tsx
+++ app/(pages)/components/topics/TopicCard.tsx
@@ -15,6 +15,7 @@ import {
   FiThumbsUp,
 } from "react-icons/fi";
 import { TopicCardProp } from "@/types";
+import UserAvatar from "@/app/components/ui/UserAvatar";
 
 const TopicCard = ({
   topic: {
@@ -23,29 +24,35 @@ const TopicCard = ({
     createdAt,
     likes,
     comments,
-    user: { firstName, middleName, lastName, profileImageUrl },
+    user: { username, firstName, middleName, lastName, profileImageUrl },
   },
 }: TopicCardProp) => {
   return (
     <div className="bg-white flex cursor-pointer transition-all duration-150 ease-in-out shadow-card hover:shadow-card-active hover:scale-102  rounded-3xl">
-      <Link
-        href={`/post/${id}`}
-        key={id}
-        className="flex gap-4 flex-1 p-6 pr-0-"
-      >
-        <Image
-          src="/images/postImage1.jpg"
-          width={170}
-          height={170}
-          alt="Post Image"
-          className="w-[9.5em] h-[9.5em] object-cover rounded-3xl"
-        />
+      <div className="flex gap-4 flex-1 p-5">
+        <Link
+          href={`/post/${id}`}
+          className="size-[7.5em] md:size-[9.5em] min-w-[7.5em] md:min-w-[9.5em] rounded-3xl block overflow-hidden"
+        >
+          <Image
+            src="/images/postImage1.jpg"
+            width={170}
+            height={170}
+            alt="Post Image"
+            className="size-full object-cover"
+          />
+        </Link>
 
-        <div className="min-w-[1em]">
-          <h5 className="font-bold text-md md:text-lg line-clamp-2 mb-2">
-            {title}
-          </h5>
-          <div className="flex items-center gap-5 md:mb-4 max-w-40">
+        <div>
+          <Link
+            href={`/post/${id}`}
+            className="hover:underline hover:text-primary"
+          >
+            <h5 className="font-bold text-md md:text-lg line-clamp-2 mb-2">
+              {title}
+            </h5>
+          </Link>
+          <div className="flex items-center gap-5 mb-2 md:mb-4 max-w-40">
             <div className="flex justify-between items-center gap-1">
               <FiThumbsUp /> <b>{likes}</b>
             </div>
@@ -54,15 +61,16 @@ const TopicCard = ({
             </div>
           </div>
 
-          <div className="flex justify-start items-center gap-3">
-            <Image
+          <Link
+            href={`/account/@${username}`}
+            className="flex justify-start items-center gap-3 w-fit hover:underline hover:text-primary"
+          >
+            <UserAvatar
               src={profileImageUrl}
-              width={200}
-              height={200}
-              alt="User porofile image"
+              alt={`${firstName?.[0]}${lastName?.[0]}`}
               className="h-7 md:h-10 w-7 md:w-10 object-cover rounded-full"
             />
-            <div className="flex flex-col justify-start items-start gap-0.5">
+            <div className="flex flex-col justify-start items-start gap-0.5 w-fit">
               <span className="text-xs md:text-sm font-bold ">
                 {firstName} {middleName} {lastName}
               </span>
@@ -70,9 +78,9 @@ const TopicCard = ({
                 {createdAt}
               </span>
             </div>
-          </div>
+          </Link>
         </div>
-      </Link>
+      </div>
       <Popover>
         <PopoverTrigger className="self-start p-4 cursor-pointer">
           <FiMoreHorizontal />
