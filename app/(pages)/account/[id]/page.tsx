e2cc1248a78364f00d2cd3b5f67d2ec52330c138
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-08-02 00:24:39 +0100

    Replace account page UI with "Coming soon" message and back button

--- app/(pages)/account/[id]/page.tsx
+++ app/(pages)/account/[id]/page.tsx
@@ -1,16 +1,13 @@
 import { getUserFromCookie } from "@/lib/auth";
-import { User } from "@/types";
-
-import ProfileHeader from "@/app/(pages)/components/account/AccountHeader";
-import PostsSection from "@/app/(pages)/components/account/PostsSection";
+import BackButton from "@/app/components/ui/BackButton";
 
 export default async function UserDashboard() {
-  const user = await getUserFromCookie();
-
   return (
     <>
-      <ProfileHeader user={user as User} />
-      <PostsSection />
+      <div className="flex items-center gap-2">
+        <BackButton />
+        <h1>Comming soon...</h1>
+      </div>
     </>
   );
 }
