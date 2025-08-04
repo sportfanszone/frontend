8a33cf13e4892280d70ca1ab0d7f3a2fb0e64e29
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-08-04 19:21:54 +0100

    chore: disable ESLint and TypeScript build errors in production

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,12 @@ const nextConfig: NextConfig = {
     ],
     dangerouslyAllowSVG: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
