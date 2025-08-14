981e3f1a1b4ea95f2235d61afa8d26c10f8113c7
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-08-14 14:20:56 +0100

    feat: implement Google callback API for user authentication

---
+++ app/api/auth/google_callback/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
+const isProd = process.env.NODE_ENV === "production";
+
+export async function GET(request: NextRequest) {
+  try {
+    const cookieStore = await cookies();
+    const { searchParams } = new URL(request.url);
+    const token = searchParams.get("token");
+
+    if (!token) {
+      return NextResponse.redirect(`${process.env.FRONTEND_URL}/auth/login`);
+    }
+
+    const response = NextResponse.redirect(
+      `${process.env.FRONTEND_URL}/user/dashboard`
+    );
+
+    cookieStore.set({
+      name: "userToken",
+      value: token!,
+      secure: isProd,
+      sameSite: isProd ? "none" : "lax",
+      path: "/",
+      maxAge: 2 * 60 * 60,
+    });
+
+    return response;
+  } catch (error) {
+    console.error("API error:", error);
+    return NextResponse.json(
+      { status: "error", message: "Invalid request" },
+      { status: 400 }
+    );
+  }
+}
