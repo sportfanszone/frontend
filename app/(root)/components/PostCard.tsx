b8d87ccde95b934f3614cecc80860db5857e5b67
Chisom Joseph Njoku <njokuchisomjoseph@gmail.com>
2025-08-31 03:14:41 +0100

    feat(PostCard): convert PostCard container to Link for improved navigation

--- app/(root)/components/PostCard.tsx
+++ app/(root)/components/PostCard.tsx
@@ -1,8 +1,9 @@
 "use client";
 import dynamic from "next/dynamic";
 import { useState } from "react";
+import Link from "next/link";
 import Image from "next/image";
-import { FiShare2, FiMessageCircle, FiThumbsUp } from "react-icons/fi";
+import { FiShare2, FiMessageCircle } from "react-icons/fi";
 import UserAvatar from "@/app/components/ui/UserAvatar";
 
 const Like = dynamic(() => import("@/app/components/ui/Like"), { ssr: false });
@@ -28,14 +29,17 @@ const PostCard = (post: PostCardProps) => {
   const [shareCount, setShareCount] = useState<number>(post?.shares || 0); // eslint-disable-line @typescript-eslint/no-unused-vars
 
   return (
-    <div className="bg-white flex items-center justify-between gap-4 w-full max-h-[8em] shadow-card hover:shadow-card-active hover:scale-102 rounded-3xl p-6 cursor-pointer transition-all duration-150 ease-in-out">
+    <Link
+      href={`/post/${post.id}`}
+      className="bg-white flex items-center justify-between gap-4 w-full max-h-[8em] shadow-card hover:shadow-card-active hover:scale-102 rounded-3xl px-2 py-2.5 cursor-pointer transition-all duration-150 ease-in-out"
+    >
       {post.images?.length > 0 ? (
         <Image
           src={post.images[0]}
           width={120}
           height={120}
           alt="Post Image"
-          className="w-[7.5em] h-[7.5em] object-cover rounded-3xl"
+          className="w-[6em] h-[6em] object-cover rounded-3xl"
         />
       ) : (
         <Image
@@ -43,10 +47,10 @@ const PostCard = (post: PostCardProps) => {
           width={120}
           height={120}
           alt="Post Image"
-          className="w-[7.5em] h-[7.5em] object-cover rounded-3xl"
+          className="w-[6em] h-[6em] object-cover rounded-3xl"
         />
       )}
-      <div className="flex-1 min-w-0 max-h-[7.5em]">
+      <div className="flex-1 min-w-0 max-h-[6em]">
         <div className="flex justify-start items-center gap-3 mb-2">
           <UserAvatar
             src={post.user.profileImageUrl}
@@ -55,9 +59,6 @@ const PostCard = (post: PostCardProps) => {
           />
           <div className="flex flex-col justify-start gap-0.5">
             <span className="text-xs font-bold">{post.user?.username}</span>
-            <span className="text-xs text-gray-500">
-              {formatDate(post.createdAt)}
-            </span>
           </div>
         </div>
 
@@ -97,7 +98,7 @@ const PostCard = (post: PostCardProps) => {
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
